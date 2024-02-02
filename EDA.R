4bb312cf0514b6a86f854fd9e0387a5ba7cbab45
laknaperera98 <laknashenaliperera@gmail.com>
2024-02-02 14:02:48 +0530

    Time Series Plots

--- EDA.R
+++ EDA.R
@@ -1,2 +1,13 @@
 data <- readxl::read_xlsx("water quality monthly variation.xlsx")
 
+# Load libraries
+library(tidyverse)
+library(ggplot2)
+
+
+# Time Series Plots
+data %>%
+  filter(`Vegetation type` == "WP") %>%
+  ggplot(aes(x=`Month/yr`, y=Av.NO3, col = as.factor(Location))) +
+  geom_line()
+  
