a030cc80ca950195240ac5f0445186c67139c3c3
sadrushi <sadrushil@gmail.com>
2024-02-05 11:21:19 +0530

    updated with new figures

--- EDA.R
+++ EDA.R
@@ -11,3 +11,39 @@ data %>%
   ggplot(aes(x=`Month/yr`, y=Av.NO3, col = as.factor(Location))) +
   geom_line()
   
+
+# Vegetation wise graphs
+
+data2 <- data %>% pivot_longer(cols = 4:18, names_to = "para_name", values_to = "para_val")
+
+data2 %>% 
+  filter(`Vegetation type` == "WP") %>%
+  ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
+  geom_line() + facet_wrap(vars(para_name), scales = "free_y")
+
+
+data2 %>% 
+  filter(`Vegetation type` == "WWI") %>%
+  ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
+  geom_line() + facet_wrap(vars(para_name), scales = "free_y")
+
+data2 %>% 
+  filter(`Vegetation type` == "CA") %>%
+  ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
+  geom_line() + facet_wrap(vars(para_name), scales = "free_y")
+
+data2 %>% 
+  filter(`Vegetation type` == "SG") %>%
+  ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
+  geom_line() + facet_wrap(vars(para_name), scales = "free_y")
+
+data2 %>% 
+  filter(`Vegetation type` == "MV") %>%
+  ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
+  geom_line() + facet_wrap(vars(para_name), scales = "free_y")
+
+data2 %>% 
+  filter(`Vegetation type` == "SV") %>%
+  ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
+  geom_line() + facet_wrap(vars(para_name), scales = "free_y")
+
