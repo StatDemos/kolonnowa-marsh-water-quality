91a14968577ac3dc1be6fa9a60804221cb93bb23
sadrushi <sadrushil@gmail.com>
2024-02-05 13:39:42 +0530

    updated code

--- EDA.R
+++ EDA.R
@@ -3,6 +3,7 @@ data <- readxl::read_xlsx("water quality monthly variation.xlsx")
 # Load libraries
 library(tidyverse)
 library(ggplot2)
+library(fpp3)
 
 
 # Time Series Plots
@@ -14,7 +15,8 @@ data %>%
 
 # Vegetation wise graphs
 
-data2 <- data %>% pivot_longer(cols = 4:18, names_to = "para_name", values_to = "para_val")
+data2 <- data %>% pivot_longer(cols = 4:18, names_to = "para_name", 
+                               values_to = "para_val")
 
 data2 %>% 
   filter(`Vegetation type` == "WP") %>%
@@ -47,3 +49,29 @@ data2 %>%
   ggplot(aes(x=`Month/yr`, y=para_val, col = as.factor(Location))) +
   geom_line() + facet_wrap(vars(para_name), scales = "free_y")
 
+##############################################
+
+
+
+data$`Month/yr` <- as.Date(data$`Month/yr`)
+
+data$`Month/yr` <-  yearmonth(data$`Month/yr`)
+
+# temp
+data <- data %>% select(Location, "Vegetation type", "Month/yr", Av.Temp) %>% 
+  as_tibble() %>% 
+  as_tsibble(index = `Month/yr`, key = Location)
+
+autoplot(data, Av.Temp) 
+
+
+# ph
+data <- data %>% select(Location, "Vegetation type", "Month/yr", Av.pH) %>% 
+  as_tibble() %>% 
+  as_tsibble(index = `Month/yr`, key = Location)
+
+autoplot(data, Av.pH) 
+
+  
+  
+  
