063d721796fb15cbe39e4f0ea5823aeed5f09c77
sadrushi <sadrushil@gmail.com>
2024-02-16 13:52:09 +0530

    ANOVA for first 7 parameters

--- ANOVA.R
+++ ANOVA.R
@@ -14,6 +14,50 @@ mean_data$`Vegetation type` <- relevel(as.factor(mean_data$`Vegetation type`), r
   
 # ANOVA
 
+# Alkalin
 data.Alkalin <- mean_data %>% filter(Parameter == "Av.Alkalin")
 model.Alkalin <- lm(Mean.Value~`Vegetation type`, data = data.Alkalin)
 summary(model.Alkalin)
+# p-value: 0.004985, significant
+
+# BOD
+data.BOD <- mean_data %>% filter(Parameter == "Av.BOD")
+model.BOD <- lm(Mean.Value~`Vegetation type`, data = data.BOD)
+summary(model.BOD)
+# p-value: 2.948e-05, significant
+
+# COD
+data.COD <- mean_data %>% filter(Parameter == "Av.COD")
+model.COD <- lm(Mean.Value~`Vegetation type`, data = data.COD)
+summary(model.COD)
+# p-value: 0.0001126, significant
+
+# DO
+data.DO <- mean_data %>% filter(Parameter == "Av.DO")
+model.DO <- lm(Mean.Value~`Vegetation type`, data = data.DO)
+summary(model.DO)
+# p-value: 0.2855, not significant
+
+# EC
+data.EC <- mean_data %>% filter(Parameter == "Av.EC")
+model.EC <- lm(Mean.Value~`Vegetation type`, data = data.EC)
+summary(model.EC)
+# p-value: 0.9794, not significant
+
+# NH4
+data.NH4 <- mean_data %>% filter(Parameter == "Av.NH4")
+model.NH4 <- lm(Mean.Value~`Vegetation type`, data = data.NH4)
+summary(model.NH4)
+# p-value: 0.000164, significant
+
+# NO3
+data.NO3 <- mean_data %>% filter(Parameter == "Av.NO3")
+model.NO3 <- lm(Mean.Value~`Vegetation type`, data = data.NO3)
+summary(model.NO3)
+# p-value: 0.00129, significant
+
+# PO4
+data.PO4 <- mean_data %>% filter(Parameter == "Av.PO4")
+model.PO4 <- lm(Mean.Value~`Vegetation type`, data = data.PO4)
+summary(model.PO4)
+# p-value: 2.991e-05, significant
